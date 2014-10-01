a1e13306bb0506ce343441cedb140aa36ae5cfc7
frgauthi <fr.gauthi@gmail.com>
2014-10-01 16:51:48 -0600

    going home part 1 is done

---
+++ part1/sched.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int current = 0;
+enum status { on, off};
+typedef enum status status;
+
+
+unsigned  i = 0;
+status flag = on;
+int pid[3] = {0};
+
+struct sigaction newhandler, oldhandler;
+sigset_t sig;
+
+void next( int code)
+{
+      kill(pid[current],SIGSTOP);
+      current++;
+      if (current == 3) current = 0;
+ 
+     kill(pid[current],SIGCONT);
+}
+
+void main()
+  {
+    // create array for 3 different children
+    
+
+    // create child process 1
+    pid[0] = fork();
+    if ( pid[0] == 0 )
+        {
+          while (1) putc('1',stderr);
+        }
+    else {
+	pid[1] = fork(); 	// creat child process 2
+	if (pid[1] == 0)
+	{
+	  while(1) putc('2',stderr);
+	}else
+	{
+		pid[2] = fork();	// create child process 3
+		if (pid[2] == 0){
+		  while(1) putc('3',stderr);
+		}
+		else{
+ 	sigemptyset(&sig);  /* clear the signal mask */
+        newhandler.sa_handler = next;
+        newhandler.sa_mask = sig;
+        newhandler.sa_flags = 0;
+
+        if ( sigaction(SIGALRM,&newhandler,&oldhandler) == -1 )
+          {
+            printf("1 - can't install signal handler \n");
+            exit(-1);
+          }
+        while (1) {
+          alarm(3);
+          pause();
+
+		}
+	}
+        	
+        }
+     }
+}
---
+++ part2/sched.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+enum status { on, off};
+typedef enum status status;
+
+enum boolean { false, true };
+typedef enum boolean boolean;
+
+unsigned  short i = 0;
+int pid;
+status flag = on;
+
+struct sigaction newhandler, oldhandler;
+sigset_t sig;
+
+void setblock( int fd ,boolean block)
+{
+   static int blockf, nonblockf;
+   static boolean first = true;
+
+   int flags;
+
+   if (first) {
+      first = false;
+      if (( flags = fcntl(fd,F_GETFL,0)) == -1)
+      {
+         fprintf(stderr," fcntl - error \n");
+         exit(1);
+      }
+     blockf = flags & ~ O_NDELAY;
+     nonblockf = flags | O_NDELAY;
+  }
+  if ( fcntl(fd,F_SETFL, block ? blockf : nonblockf) == -1 )
+      {
+         fprintf(stderr," fcntl2 - error \n");
+         exit(1);
+      }
+}
+void next( int code)
+{
+  static status flag = on;
+
+  if (flag == on) {
+      kill(pid,SIGSTOP);
+      flag = off;
+  } else {
+     kill(pid,SIGCONT);
+     flag = on;
+ }
+}
+
+void main()
+   {
+    char mess[80];
+    int fd;
+    int numch;
+
+    pid = fork();
+    if ( pid == 0 )
+        {
+          while (1) 
+             { 
+              i++;
+              if (i % 10000 == 0 ) putc('.',stderr); 
+             }
+        }
+    else { 
+
+        fd = open("/dev/tty",O_RDWR);
+        setblock(fd,false);
+
+        sigemptyset(&sig);  /* clear the signal mask */
+        newhandler.sa_handler = next;
+        newhandler.sa_mask = sig;
+        newhandler.sa_flags = 0;
+
+        if ( sigaction(SIGALRM,&newhandler,&oldhandler) == -1 )
+          {
+            printf("1 - can't install signal handler \n");
+            exit(-1);
+          }
+        while (1) {
+          alarm(3);
+          pause();
+          switch  ( numch = read(fd,mess,80))  {
+          case -1 :
+          case 0 :
+              break;
+          default: 
+	      fprintf(stderr," %d   <%s>\n",numch,mess);
+          }
+          fprintf(stderr," in parent\n");
+        }
+     }
+}
